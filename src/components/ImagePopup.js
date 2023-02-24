cbe19790b57392f5096dcb3f263920777e633e20
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-02-24 10:52:11 +0300

    fix: починить вэбпак

---
+++ src/components/ImagePopup.js
@@ -0,0 +1,11 @@
+export default function ImagePopup({ card, onClose, onOutPopupClick }) {
+  return (
+    <div className={`popup popup_card ${card.isOpen ? 'popup_opened-card' : ''}`} onClick={onOutPopupClick}>
+      <div className="popup__container">
+        <img src={card.link} alt={`Изображение ${card.name}`} className="popup__image" />
+        <figcaption className="popup__label">{card.name}</figcaption>
+        <button type="button" className="popup__close-button" aria-label="Закрыть" onClick={onClose}></button>
+      </div>
+    </div>
+  )
+}
---
+++ src/utils/Api.js
@@ -0,0 +1,79 @@
+import {url,
+        userAuthData} from './constants';
+class Api {
+  constructor(url, { userToken, cohortName }) {
+    this._url = url;
+    this._userToken = userToken;
+    this._cohort = cohortName;
+  }
+
+  _processResponse(response, error) {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(error);
+  }
+
+  _createSimpleRequest(extensionUrl, requestMethod, errorText) {
+    this._completeUrl = `${this._url}/${this._cohort}`.concat(extensionUrl);
+    return fetch(this._completeUrl, {
+      method: requestMethod,
+      headers: {
+        'authorization': `${this._userToken}`
+      }
+    })
+    .then(response => this._processResponse(response, errorText))
+  }
+
+  _createBodyRequest(extensionUrl, requestMethod, errorText, bodyData) {
+    this._completeUrl = `${this._url}/${this._cohort}`.concat(extensionUrl);
+    return fetch(this._completeUrl, {
+      method: requestMethod,
+      headers: {
+
+        'authorization': `${this._userToken}`,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(bodyData)
+    })
+    .then(response => this._processResponse(response, errorText))
+  }
+
+  getUserInfo() {
+    return this._createSimpleRequest('/users/me', 'GET', 'Ошибка авторизации')
+  }
+
+  getCardsData() {
+    return this._createSimpleRequest('/cards', 'GET', 'Ошибка обновления карточек')
+  }
+
+  deleteCardData(cardId) {
+    return this._createSimpleRequest(`/cards/${cardId}`, 'DELETE', 'Ошибка удаления карточки')
+  }
+
+  updateAvatarData(formData) {
+    return this._createBodyRequest('/users/me/avatar', 'PATCH', 'Ошибка обновления аватара', formData)
+  }
+
+  sendCardData(formData) {
+    return this._createBodyRequest('/cards', 'POST', 'Ошибка добавления карточки', formData)
+  }
+
+  updateProfileData(formData) {
+    return this._createBodyRequest('/users/me', 'PATCH', 'Ошибка обновления профиля', formData)
+  }
+
+  addLikeToCard(cardId) {
+    return this._createSimpleRequest(`/cards/${cardId}/likes`, 'PUT', 'Ошибка добавления лайка')
+  }
+
+  delLikeToCard(cardId) {
+    return this._createSimpleRequest(`/cards/${cardId}/likes`, 'DELETE', 'Ошибка удаления лайка')
+  }
+}
+
+const api = new Api(url, userAuthData);
+
+export {api};
+
+
